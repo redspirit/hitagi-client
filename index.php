e0ebe4f282cd98d71763419db8498fa3f3fb266c
redspirit <redspirit2@ya.ru>
2013-01-08 17:45:14 +0800

    remove anti cache
    Убрал ?time() с концов JS файлов

--- index.php
+++ index.php
@@ -7,24 +7,24 @@
 <head>
 <meta charset="utf-8" />
 <title>Hitagi Chat</title>
-	<link rel="stylesheet" type="text/css" href="css/mark.css?<?php echo time(); ?>" />
+	<link rel="stylesheet" type="text/css" href="css/mark.css?<?= time() ?>" />
 	<link rel="stylesheet" media="screen" type="text/css" href="js/colorpicker/css/colorpicker.css" />
-<!--	<script src="http://<?php echo $serverip; ?>:8080/socket.io/socket.io.js"></script> -->
+<!--	<script src="http://<?= $serverip ?>:8080/socket.io/socket.io.js"></script> -->
 	<script type="text/javascript" src="js/socket.io.js"></script>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/hitagi.js?<?php echo time(); ?>"></script>
-	<script type="text/javascript" src="js/tools.js?<?php echo time(); ?>"></script>
+	<script type="text/javascript" src="js/hitagi.js"></script>
+	<script type="text/javascript" src="js/tools.js"></script>
 	<script type="text/javascript" src="http://userapi.com/js/api/openapi.js?1"></script>
 	
 <script type="text/javascript">
-	var serverIp = '<?php echo $serverip; ?>';
-	var VKAPIID = <?php echo $VKAPIID; ?>;
+	var serverIp = '<?= $serverip; ?>';
+	var VKAPIID = <?= $VKAPIID; ?>;
 	var currentRoom = 'public';
 	soundManager.url = '/hitagi-client/sounds/soundmanager2.swf';
 	var ch = hitagiCreate(serverIp, 8080);
 
 </script>	
-<script type="text/javascript" src="js/chatcore.js?<?php echo time(); ?>"></script>
+<script type="text/javascript" src="js/chatcore.js?<?= time(); ?>"></script>
 </head>
 <body>
 <div id="head">
@@ -97,7 +97,7 @@
 </table>
 </div>
 
-<div id="widget-radio"></div>
+<!-- <div id="widget-radio"></div> -->
 <div id="widget-help"></div>
 
 
