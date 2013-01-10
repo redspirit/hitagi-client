ff02c0b80ac45028f37d6ba115c28a3370b06a5b
redspirit <redspirit2@ya.ru>
2013-01-10 19:15:29 +0800

    little bugfix

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 <head>
 <meta charset="utf-8" />
 <title>Аниме чат Hitagi</title>
-	<link rel="stylesheet" type="text/css" href="themes/jquery.jscrollpane.css" />
 	<link rel="stylesheet" type="text/css" href="themes/<?= $style ?>" />
 	<link rel="stylesheet" media="screen" type="text/css" href="js/colorpicker/css/colorpicker.css" />
 <!--	<script src="http://<?= $serverip ?>:8080/socket.io/socket.io.js"></script> -->
@@ -22,7 +21,6 @@
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/hitagi.js"></script>
 	<script type="text/javascript" src="js/tools.js"></script>
-	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>	
 	<script type="text/javascript" src="http://userapi.com/js/api/openapi.js?1"></script>
 	
 <script type="text/javascript">
